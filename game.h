399ec0a8bc3a7f97358cf690d6274296a59b23a6
Brigid O'Keefe <51684952+BrigidOKeefe@users.noreply.github.com>
2021-04-28 15:27:46 -0400

    Add files via upload

--- game.h
+++ game.h
@@ -13,13 +13,21 @@ struct backpack
 //create new bag
 struct backpack new_bag();
 
-
 //function to create random number
 int rand_in_range(int x, int y); 
 
 //function to create a random list of numbers that doesn't repeat
 int* rand_list(int *list, int length, int x, int y);
 
+//function to print section of text separated by *
 void print_section(FILE *fptr);
+
+//function to print next scene
+void print_scene(FILE *fptr, char *item);
+
+//function to select next item
+int pick_item(int answer);
+
+
 
 #endif
